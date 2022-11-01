1965ce52b13666a587f91ffdd23351230ab75bbf
Liquid Monks <info@liquidmonks.com>
2022-11-01 17:47:23 -0500

    script.js includes a password parameter number array with numerical elements (0-9)

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+//This password generator shall allow a user to generate a password between 8-50 characters with a minimum of one predefined parameters (i.e. numerical length, special characters, and/or letter case). 
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// Password Parameter Arrays
+let number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',]; // array lists all available numerical values
+
 
 // Write password to the #password input
 function writePassword() {
