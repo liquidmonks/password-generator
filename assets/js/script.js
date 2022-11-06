3c9cd74baf49571aacce71fbc272a7cf9fd69888
Liquid Monks <info@liquidmonks.com>
2022-11-06 10:45:41 -0600

    script.js includes new function structure with clear code flow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,34 @@
-//This password generator shall allow a user to generate a password between 8-50 characters with a minimum of one predefined parameters (i.e. numerical length, special characters, and/or letter case). 
+//This strong password generator shall allow a user to generate a password between 8-128 characters with a minimum of one predefined parameters (i.e. numerical length, special characters, and/or letter case). 
+
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
+
+
 // Global Variables
 
 // This is my database: numbers, special characters, lowercase letters, uppercase letters
 let number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; // create a number array with elements (numbers) 0-9
-let passwordLength = 5;
-let randomInt = getRandomArbitrary(10);
-console.log(randomInt);
-let password = number[randomInt];
 
-// Sets criteria for password (object):
+// Creates a password object:
 
-let passwordCriteria = {
+let passwordParameter = {
 
-  //Sets property:value for password length:
+  // Sets property:value for password length:
   passwordLength: 0
 
-  //TODO: add property:value for password Numbers
-  //TODO: add property:value for password specialCharacters
-  //TODO: add property:value for password alphaLower
-  //TODO: add property:value for password alphaUpper
+  // TODO: add property:value arrays for password numbers 0 - 9 (passwordNumbers)
+  //  use - let passwordNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];,
 
+  // TODO: add property:value arrays for password special characters (passwordSpecialChars)
+  // use - let passwordSpecialChars = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];,
 
+  // TODO: add property:value arrays for password alphabet lower characters (passwordLowerChars),
+  // use - let passwordLowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];,
 
+  // TODO: add property:value arrays for password alphabet upper characters (passwordUpperChars)
+  // user - let passwordUpperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 }
 
-
-
-// Assignment Code
-let generateBtn = document.querySelector("#generate");
 
 
 // This function will generate the password and return it to the correct password
@@ -45,8 +46,11 @@ function getRandomArbitrary(max) {
 function writePassword() {
   let password = generatePassword();
 
-  // Sets ID of the password writting passwordText = to the textArea in index.html file
+
+  // Sets ID of the password writing passwordText = to the textArea in index.html file
   let passwordText = document.querySelector("#password");
+
+
   // Updates the textArea with a new password
   passwordText.value = password;
 
@@ -54,3 +58,35 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Function where the sausage is made to generate a new password
+function generatePassword() {
+
+  // Sausage casing that temporarily holds the password the generator outputs to the user:
+  let passwordResult = "";
+
+  // Password parameter selection prompts that records input from the user:
+
+  let passwordLength = 0;
+  // TODO: add the following user prompt - "let numbers;""
+  // TODO: add the following user prompt - "let upperCase;""
+  // TODO: add the following user prompt - "let lowerCase;""
+  // TODO: add the following user prompt - "let specialChars;""
+
+}
+
+// Load characters:
+passwordLength = 0;
+passwordParameter.passwordLength = 0;
+result = "";
+
+// Quality control test for password parameters: 
+// TODO: add all the while, if, else, and return statement for each parameter condition
+
+
+// Returns the password from the sausage casing back to the call function
+//  TODO: add - "return result;""
+
+// Sausage making process functions that displays user parameter selection prompts;
+// TODO: add function to show user prompts - use function showPrompts() {........} and confirm
