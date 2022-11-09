4637cd34e6f0195f7218dbf8397be92ff13f809d
Liquid Monks <info@liquidmonks.com>
2022-11-08 21:46:49 -0600

    script.js includes a SpecialChars array that holds special characters for an additional user choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,34 @@
-//This secure password generator shall allow a user to generate a password between 8-128 characters with a minimum of one predefined parameters (i.e. numerical length, special characters, and/or letter case). 
-
+//This secure password generator shall allow a user to generate a password between 8-128 characters with a minimum of one predefined parameters (i.e. numerical length, special characters, and/or letter case).
 
 // Global Variables
 
 // This is my database: numbers, lowercase letters, uppercase letters, special characters
-// * parameter array for numbers - let passNumber = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; 
+// * parameter array for numbers - let passNumber = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 // * parameter array for lowercase letters - let passLowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];,
 // * parameter array for uppercase letters - let passUpperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 // * parameter array for Special Characters - let passSpecialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
-
 
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
 // Creates a password object:
 let passParameter = {
-
   // Sets property:value for password length:
   passLength: 0,
-
 
   // Sausage casing array that holds password numbers parameter:
   passNumber: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
 
   // TODO: add property:value arrays for password special characters (passSpecialChar)
-  // use - let passSpecialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];,
+  // use - passSpecialChar: ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];,
+  passSpecialChar: ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"], // Array holds special characters values
 
   // TODO: add property:value arrays for password alphabet lower characters (passLowerChar),
-  // use - let passdLowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];,
+  // use - passLowerChar:["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];,
 
-  // TODO: add property:value arrays for password alphabet upper characters (passdUpperChar)
-  // user - let passUpperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-}
+  // TODO: add property:value arrays for password alphabet upper characters (passUpperChar)
+  // use - passUpperChar: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+};
 
 // Write password to the #password input of index.html file
 function writePassword() {
@@ -51,7 +47,6 @@ generateBtn.addEventListener("click", writePassword);
 
 // Function where the sausage is made to generate a new password
 function generatePassword() {
-
   // Sausage casing that temporarily holds the password the generator outputs to the user:
   let passResult = "";
 
@@ -65,13 +60,12 @@ function generatePassword() {
   // TODO: add the following user prompt - "let lowerCase;""
   // TODO: add the following user prompt - "let specialChar;""
 
-
   //initialize characters
   passwordLength = 0;
   passParameter.passLength = 0;
   passResult = "";
 
-  // Quality control test for password parameters: 
+  // Quality control test for password parameters:
   // TODO: add all the while, if, else, and return statement for each parameter condition
   while (passwordLength < 8 || passwordLength > 128) {
     passwordLength = prompt("How many characters do you want your password to be? \nPassword must be between 8 and 128 characters.");
@@ -79,37 +73,33 @@ function generatePassword() {
     // TODO: add an if statement if user cancels prompt, then return a confirmation statement
     if (passwordLength === null) {
       return "Your secure password";
-    }
-    else {
+    } else {
       // validates that user has entered an integer
       if (!isFinite(passwordLength)) {
         alert("You did not enter a number");
         return "Your secure password";
-      }
-      else {
+      } else {
         //       validates password meets length parameter
         if (passwordLength < 8 || passwordLength > 128) {
           alert("Password must be between 8 and 128 characters.");
           return "Your secure password";
-        }
-        else {
-
+        } else {
           //call the internal function to show prompts for criteria
           showPrompts();
 
           //         continues to add parameters until passwordLength = user entered value
           while (passParameter.passLength < passwordLength) {
-            //if statement to make sure the user selects at least one of the criteria  
+            //if statement to make sure the user selects at least one of the criteria
             if (numbers === false) {
-              alert("You have selected numbers")
+              alert("You have selected numbers");
               showPrompts();
             }
 
             //if the user selected a number and there is still room to add characters then
-            //randomly grab a number from the array and add it to the end of passResult 
+            //randomly grab a number from the array and add it to the end of passResult
             //update passLength by 1
             if (numbers === true && passParameter.passLength < passwordLength) {
-              let num = passParameter.passNumber[Math.floor(Math.random() * 10)]
+              let num = passParameter.passNumber[Math.floor(Math.random() * 10)];
               passResult = passResult + num;
               passParameter.passLength++;
 
