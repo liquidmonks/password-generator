2a067019f585cbef54ec532c68699e8bd26b927f
Liquid Monks <info@liquidmonks.com>
2022-11-09 17:29:49 -0600

    The variable passLowerChar is now arrayNumber in script.js to add clarity to purpose of this variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
 
 // This is my database: numbers, lowercase letters, uppercase letters, special characters
 // * parameter array for numbers - let arrayNumber = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-// * parameter array for lowercase letters - let passLowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];,
+// * parameter array for lowercase letters - let arrayLowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];,
 // * parameter array for uppercase letters - let passUpperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 // * parameter array for Special Characters - let passSpecialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
@@ -22,8 +22,8 @@ let passParameter = {
   // Sausage casing array that holds password special characters parameter:
   passSpecialChar: ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"], // Array holds special characters values
 
-  // TODO: add property:value arrays for password alphabet lower characters (passLowerChar),
-  // use - passLowerChar:["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];,
+  // TODO: add property:value arrays for password alphabet lower characters (arrayLowerChar),
+  // use - arrayLowerChar:["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];,
 
   // TODO: add property:value arrays for password alphabet upper characters (passUpperChar)
   // use - passUpperChar: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
